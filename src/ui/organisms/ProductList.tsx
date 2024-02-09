cd05f500e1f1ff60b1b55e90c217853b8d818a59
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2024-02-09 12:33:09 +0100

    added cursor pointer to product list items

--- src/ui/organisms/ProductList.tsx
+++ src/ui/organisms/ProductList.tsx
@@ -2,7 +2,7 @@ import { type ProductItemType } from "@/ui/types";
 import { ProductListItem } from "@/ui/molecules/ProductListItem";
 
 export const ProductList = ({ products }: { products: ProductItemType[] }) => (
-	<ul className="grid grid-cols-1 gap-8 sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+	<ul className="grid cursor-pointer grid-cols-1 gap-8 sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
 		{products.map((product) => (
 			<ProductListItem key={product.id} product={product} />
 		))}
