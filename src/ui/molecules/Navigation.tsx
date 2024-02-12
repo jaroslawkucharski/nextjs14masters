e67a8ea23d71c428b0146d4d33d81cafdc48ae40
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2024-02-12 18:03:57 +0100

    updated Navigation styling to center content

--- src/ui/molecules/Navigation.tsx
+++ src/ui/molecules/Navigation.tsx
@@ -1,7 +1,7 @@
 import { ActiveLink } from "@/ui/atoms/ActiveLink";
 
 export const Navigation = () => (
-	<nav className="flex h-fit gap-4 px-4">
+	<nav className="flex h-fit content-center justify-center gap-4 px-4">
 		<ActiveLink href="/">Home</ActiveLink>
 		<ActiveLink href="/products">Products</ActiveLink>
 	</nav>
