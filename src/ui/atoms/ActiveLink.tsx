1a03a2c3129b1a87a49047f109e60d5c6945cfa7
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2024-02-12 21:51:12 +0100

    fixed type error in ActiveLink component

--- src/ui/atoms/ActiveLink.tsx
+++ src/ui/atoms/ActiveLink.tsx
@@ -1,18 +1,19 @@
 "use client";
 
+import { type UrlObject } from "url";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
 import { type ReactNode } from "react";
 
 type ActiveLinkProps = {
-	href: string;
+	href: UrlObject;
 	children: ReactNode;
 };
 
 export const ActiveLink = ({ href, children }: ActiveLinkProps) => {
 	const pathname = usePathname();
-	const isActive = pathname === href;
+	const isActive = pathname === String(href);
 
 	return (
 		<Link
