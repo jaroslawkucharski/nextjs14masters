70171a1da4263ca3821b612944f27d61c058789d
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2024-02-13 23:04:12 +0100

    test fixed

--- src/ui/atoms/Pagination.tsx
+++ src/ui/atoms/Pagination.tsx
@@ -6,21 +6,20 @@ export const Pagination = ({ length }: { length: number }) => {
 	console.log("numOfPages: ", numOfPages);
 
 	return (
-		<ul
-			className="mt-20 flex w-full justify-center gap-4"
-			aria-label="pagination"
-		>
-			{Array.from({ length: numOfPages }, (_, index) => index + 1).map(
-				(page, pageIndex) => (
-					<li key={page}>
-						<ActiveLink className="text-lg" href={`/products/${page}`}>
-							{page}
-						</ActiveLink>
+		<nav>
+			<ul className="mt-20 flex w-full justify-center" aria-label="pagination">
+				{Array.from({ length: numOfPages }, (_, index) => index + 1).map(
+					(page, pageIndex) => (
+						<li key={page} className="flex justify-center gap-4">
+							<ActiveLink className="text-lg" href={`/products/${page}`}>
+								{page}
+							</ActiveLink>
 
-						{pageIndex !== numOfPages - 1 && <span>|</span>}
-					</li>
-				),
-			)}
-		</ul>
+							{pageIndex !== numOfPages - 1 && <span>|</span>}
+						</li>
+					),
+				)}
+			</ul>
+		</nav>
 	);
 };
