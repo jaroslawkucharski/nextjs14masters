128b4f170581ef26bea0d9ef82e0d24207861e46
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2024-02-13 23:17:22 +0100

    test fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Lato } from "next/font/google";
 import "./globals.css";
-import { Header } from "@/ui/organisms/Header";
+// import { Header } from "@/ui/organisms/Header";
+import { Navigation } from "@/ui/molecules/Navigation";
 
 const lato = Lato({
 	subsets: ["latin"],
@@ -21,7 +22,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={lato.className}>
-				<Header />
+				<Navigation />
 
 				<section className="mx-auto max-w-md p-12 sm:max-w-2xl sm:py-16 md:max-w-4xl lg:max-w-7xl">
 					{children}
