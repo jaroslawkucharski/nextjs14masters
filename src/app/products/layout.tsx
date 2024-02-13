46e3a9c7b8ccad2f915a383b62b05620a7415dea
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2024-02-13 22:58:58 +0100

    test fixed

--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -1,6 +1,6 @@
 import { type Metadata } from "next";
 import { type ReactNode } from "react";
-import { Pagination } from "@/ui/atoms/Paginaation";
+import { Pagination } from "@/ui/atoms/Pagination";
 import { getProducts } from "@/api/products";
 
 export const metadata: Metadata = {
--- src/ui/atoms/Pagination.tsx
+++ src/ui/atoms/Pagination.tsx
@@ -3,6 +3,8 @@ import { ActiveLink } from "./ActiveLink";
 export const Pagination = ({ length }: { length: number }) => {
 	const numOfPages = Math.ceil(length / 20);
 
+	console.log("numOfPages: ", numOfPages);
+
 	return (
 		<ul
 			className="mt-20 flex w-full justify-center gap-4"
@@ -10,15 +12,13 @@ export const Pagination = ({ length }: { length: number }) => {
 		>
 			{Array.from({ length: numOfPages }, (_, index) => index + 1).map(
 				(page, pageIndex) => (
-					<>
-						<li key={page}>
-							<ActiveLink className="text-lg" href={`/products/${page}`}>
-								{page}
-							</ActiveLink>
-						</li>
+					<li key={page}>
+						<ActiveLink className="text-lg" href={`/products/${page}`}>
+							{page}
+						</ActiveLink>
 
 						{pageIndex !== numOfPages - 1 && <span>|</span>}
-					</>
+					</li>
 				),
 			)}
 		</ul>
