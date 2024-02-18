8e468033dd2689dd468c2e0fe46adb41d422e5ab
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2024-02-18 16:58:03 +0100

    added rating to product list item description

--- src/ui/atoms/ProductListItemDescription.tsx
+++ src/ui/atoms/ProductListItemDescription.tsx
@@ -1,3 +1,4 @@
+import { Star } from "lucide-react";
 import { formatMoney } from "@/utils";
 import { type ProductListItemFragment } from "@/gql/graphql";
 
@@ -19,7 +20,20 @@ export const ProductListItemDescription = ({
 			{categories[0]?.name || ""}
 		</p>
 
-		<p className="text-md self-end pt-1 font-medium text-gray-900">
+		<p className="text-md flex w-full justify-between self-end pt-1 font-medium text-gray-900">
+			<span className="sr-only">Rating:</span>
+
+			<span className="flex gap-2">
+				<span className="self-center text-xs">4.7 / 5</span>
+				<span className="flex justify-end self-center">
+					<Star className="h-4 w-4 fill-current text-yellow-400" />
+
+					<Star className="h-4 w-4 fill-current text-yellow-400" />
+
+					<Star className="h-4 w-4 fill-current text-yellow-400" />
+				</span>
+			</span>
+
 			<span className="sr-only">Price:</span>
 
 			{formatMoney(price)}
