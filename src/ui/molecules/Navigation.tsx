b171b0a375a672814300c08ca177fe084723496c
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2024-02-18 17:06:04 +0100

    added searcher to navigation

--- src/ui/molecules/Navigation.tsx
+++ src/ui/molecules/Navigation.tsx
@@ -27,7 +27,7 @@ const navigationLinks = [
 ];
 
 export const Navigation = () => (
-	<nav className="hidden h-fit flex-1 justify-between gap-8 self-center px-4 sm:flex">
+	<nav className="hidden h-fit flex-1 justify-between gap-8 self-center px-4 xl:flex">
 		<ul className="hidden gap-8 px-4 sm:flex">
 			{navigationLinks.map(({ label, href, exact }) => (
 				<li key={label}>
@@ -38,12 +38,21 @@ export const Navigation = () => (
 			))}
 		</ul>
 
-		<div className="relative">
-			<ShoppingCart />
+		<div className="flex gap-8">
+			<input
+				className="w-full min-w-64 self-center rounded-md border-0 bg-slate-50 py-2 pl-4 pr-4 text-sm text-slate-800 ring-1 ring-inset ring-gray-300 placeholder:text-slate-400 focus:ring-2 focus:ring-inset focus:ring-blue-400"
+				placeholder="Search"
+				type="search"
+				name="search"
+			/>
 
-			<span className="absolute bottom-4 left-4 rounded-full bg-red-500 px-1 text-xs text-white">
-				0
-			</span>
+			<div className="relative self-center">
+				<ShoppingCart />
+
+				<span className="absolute bottom-4 left-4 rounded-full bg-red-500 px-1 text-xs text-white">
+					0
+				</span>
+			</div>
 		</div>
 	</nav>
 );
