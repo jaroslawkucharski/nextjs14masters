774f0846d820b240eaaf61cb76fa11c8a60f4410
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2024-02-19 17:34:36 +0100

    changed category paths

--- src/app/categories/[category]/layout.tsx
+++ src/app/categories/[category]/layout.tsx
@@ -17,7 +17,7 @@ export default async function CategoryLayout({
 
 	return (
 		<>
-			<PageHeading>{category.name}</PageHeading>
+			<PageHeading title={category.name} description={category.description} />
 
 			<section className="mx-auto max-w-md p-12 sm:max-w-2xl sm:py-16 md:max-w-4xl lg:max-w-7xl">
 				{children}
