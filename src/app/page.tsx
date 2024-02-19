6654b0c17f6f9a853c17c1cca742ddd70dc0c9e9
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2024-02-19 17:34:12 +0100

    updated page and layout headings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
 
 	return (
 		<>
-			<PageHeading>Home</PageHeading>
+			<PageHeading>COLLECTIONS LIST</PageHeading>
 
 			<section className="mx-auto max-w-md p-12 sm:max-w-2xl sm:py-16 md:max-w-4xl lg:max-w-7xl">
 				<ProductsList products={products} />
--- src/app/products/[[...page]]/layout.tsx
+++ src/app/products/[[...page]]/layout.tsx
@@ -35,7 +35,7 @@ export default async function ProductsLayout({
 
 	return (
 		<>
-			<PageHeading>All products</PageHeading>
+			<PageHeading title="All products" />
 
 			<section className="mx-auto max-w-md p-12 sm:max-w-2xl sm:py-16 md:max-w-4xl lg:max-w-7xl">
 				{children}
