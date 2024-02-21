ebc383b26f0c6ab28f7551f564f707bfa6bc310f
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2024-02-21 14:47:55 +0100

    refactored Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,41 @@
-import { getProductList } from "@/api/products";
+import { getCollectionsList } from "@/api/getCollectionList";
+import { getProductList } from "@/api/getProductList";
 import { PageHeading } from "@/ui/atoms/PageHeading";
+import { CollectionList } from "@/ui/organisms/CollectionList";
 import { ProductsList } from "@/ui/organisms/ProductList";
 
+export async function generateStaticParams() {
+	const { products } = await getProductList({
+		take: 4,
+		orderBy: "PRICE",
+	});
+
+	const collections = await getCollectionsList({});
+
+	return {
+		products,
+		collections,
+	};
+}
+
 export default async function Home() {
-	const { products } = await getProductList({ take: 4 });
+	const { products } = await getProductList({
+		take: 4,
+		orderBy: "PRICE",
+	});
+
+	const collections = await getCollectionsList({});
 
 	return (
 		<>
-			<PageHeading>COLLECTIONS LIST</PageHeading>
+			<PageHeading>
+				<CollectionList collections={collections} />
+			</PageHeading>
 
-			<section className="mx-auto max-w-md p-12 sm:max-w-2xl sm:py-16 md:max-w-4xl lg:max-w-7xl">
+			<section
+				className="mx-auto max-w-md p-12 sm:max-w-2xl sm:py-16 md:max-w-4xl lg:max-w-7xl"
+				data-testid="related-products"
+			>
 				<ProductsList products={products} />
 			</section>
 		</>
