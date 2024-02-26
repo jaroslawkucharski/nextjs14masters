ec1f0ff7ae3d68421bf1df934e6ac0e14eb64907
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2024-02-26 14:27:19 +0100

    refactored ProductCounter component to use initial values

--- src/ui/atoms/ProductCounter.tsx
+++ src/ui/atoms/ProductCounter.tsx
@@ -1,40 +1,37 @@
 "use client";
 
-import { type ReactNode, useState } from "react";
+import { useState } from "react";
 
 type ProductCounterProps = {
-	children: ReactNode;
+	values: number;
 };
 
-export const ProductCounter = ({ children }: ProductCounterProps) => {
-	const [counter, setCounter] = useState(0);
+export const ProductCounter = ({ values }: ProductCounterProps) => {
+	const [counter, setCounter] = useState(values);
 
 	return (
-		<div className="w-full p-6">
+		<div className="w-full">
 			<div className="flex flex-nowrap">
 				<button
-					onClick={() => setCounter((counter) => counter + 1)}
-					className="border bg-gray-950 px-4 text-xl text-white"
+					onClick={() => counter > 1 && setCounter((counter) => counter - 1)}
+					className="w-8 border bg-gray-950 text-center text-lg text-white"
 				>
-					+
+					-
 				</button>
 
 				<input
 					readOnly
 					value={counter}
-					className="w-full border border-slate-200 px-2 text-center text-xl"
+					className="w-16 border border-slate-200 px-2 text-center text-lg"
 				/>
 
 				<button
-					onClick={() => setCounter((counter) => counter - 1)}
-					className="border bg-gray-950 px-4 text-center text-xl text-white"
+					onClick={() => setCounter((counter) => counter + 1)}
+					className="w-8 border bg-gray-950 text-center text-lg text-white"
 				>
-					-
+					+
 				</button>
 			</div>
-
-			{/* TODO - test */}
-			{counter % 2 === 0 && children}
 		</div>
 	);
 };
