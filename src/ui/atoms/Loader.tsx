b7f54ad65c6050a6a25c5876c248823b81ca9bdf
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2024-02-27 09:41:19 +0100

    updated Loader component styles

--- src/ui/atoms/Loader.tsx
+++ src/ui/atoms/Loader.tsx
@@ -7,10 +7,10 @@ type LoaderProps = {
 export const Loader = ({ size = "lg" }: LoaderProps) => (
 	<div
 		className={clsx(
-			"flex animate-spin items-center justify-center rounded-full border-gray-200 border-t-transparent",
+			"flex animate-spin items-center justify-center rounded-full border-t-transparent",
 			{
-				["h-20 w-20 border-8"]: size === "lg",
-				["h-5 w-5 border-2"]: size === "sm",
+				["h-20 w-20 border-8 border-gray-500"]: size === "lg",
+				["h-5 w-5 border-2 border-gray-200"]: size === "sm",
 			},
 		)}
 		aria-busy="true"
