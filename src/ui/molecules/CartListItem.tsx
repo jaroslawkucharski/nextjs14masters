a8a5e59e50d1937dc144d1aa850492b86d6b382d
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2024-02-29 08:43:45 +0100

    created CartListItem component to display items in the cart

---
+++ src/ui/molecules/CartListItem.tsx
@@ -0,0 +1,58 @@
+import Link from "next/link";
+import Image from "next/image";
+import { ProductCounter } from "./ProductCounter";
+import { RemoveProductFromCart } from "./RemoveProductFromCart";
+import { type Cart } from "@/gql/graphql";
+import { formatMoney } from "@/utils";
+
+export const CartListItem = ({
+	item,
+	cartId,
+}: {
+	item: Cart["items"][0];
+	cartId: string;
+}) => (
+	<tr key={item.product.id} className="h-fit border-b">
+		<td className="invisible py-4 sm:visible sm:min-w-48 sm:px-4">
+			{item.product.images?.[0] && (
+				<Link
+					prefetch
+					href={`/product/${item.product.slug}-${item.product.id}`}
+				>
+					<Image
+						src={item.product.images[0].url}
+						alt={item.product.name}
+						width={150}
+						height={150}
+					/>
+				</Link>
+			)}
+		</td>
+
+		<td className="w-full px-4 py-8">
+			<p className="mb-10 text-xl">
+				<Link
+					className="h-fit hover:underline"
+					prefetch
+					href={`/product/${item.product.slug}-${item.product.id}`}
+				>
+					{item.product.name}
+				</Link>
+			</p>
+
+			<div className="pr-8 text-sm text-gray-600">
+				<ProductCounter
+					id={cartId}
+					quantity={item.quantity}
+					productId={item.product.id}
+				/>
+			</div>
+		</td>
+
+		<td className="self-end px-4 py-8">
+			<RemoveProductFromCart cartId={cartId} productId={item.product.id} />
+
+			<p>{formatMoney(item.product.price * item.quantity)}</p>
+		</td>
+	</tr>
+);
