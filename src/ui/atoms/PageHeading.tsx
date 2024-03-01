9c0969d4401f6308d58c6c6764fae6cd9c233a50
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2024-03-01 13:06:59 +0100

    refactored PageHeading component styling

--- src/ui/atoms/PageHeading.tsx
+++ src/ui/atoms/PageHeading.tsx
@@ -11,12 +11,8 @@ export const PageHeading = ({
 	description,
 	children,
 }: PageHeadingProps) => (
-	<div className="bg-slate-100 px-2 pb-10 pt-10 text-center sm:px-36 sm:text-left lg:pt-28">
-		{title && (
-			<h2 className="text-lg font-semibold md:text-left lg:text-left">
-				{title}
-			</h2>
-		)}
+	<div className="bg-slate-50 px-2 pb-10 pt-10 text-center sm:px-36 lg:pt-28 lg:text-left">
+		{title && <h2 className="text-2xl font-semibold">{title}</h2>}
 
 		{description && <p className="text-gray-600">{description}</p>}
 
