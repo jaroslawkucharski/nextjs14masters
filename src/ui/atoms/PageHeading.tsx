c1148e7049ddb63d2851c8c1e0075c16cc5c3030
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2024-03-02 12:04:44 +0100

    added sort functionality to PageHeading component

--- src/ui/atoms/PageHeading.tsx
+++ src/ui/atoms/PageHeading.tsx
@@ -1,18 +1,25 @@
 import { type ReactNode } from "react";
+import { Sort } from "@/ui/molecules/Sort";
 
 type PageHeadingProps = {
 	title?: string;
 	description?: string;
 	children?: ReactNode;
+	sort?: boolean;
 };
 
 export const PageHeading = ({
 	title,
 	description,
 	children,
+	sort,
 }: PageHeadingProps) => (
-	<div className="bg-slate-50 px-2 pb-10 pt-10 text-center sm:px-36 lg:pt-28 lg:text-left">
-		{title && <h2 className="text-2xl font-semibold">{title}</h2>}
+	<div className="bg-slate-50 px-8 pb-10 pt-10 text-center sm:px-36 lg:pt-28 lg:text-left">
+		<div className="flex flex-wrap items-center justify-center gap-4 sm:justify-between">
+			{title && <h2 className="text-2xl font-semibold">{title}</h2>}
+
+			{sort && <Sort />}
+		</div>
 
 		{description && <p className="text-gray-600">{description}</p>}
 
