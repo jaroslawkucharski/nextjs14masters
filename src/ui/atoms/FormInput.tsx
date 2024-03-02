63b11dcb1fd941e567ab2121392e6f9f79980ea8
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2024-03-02 12:04:38 +0100

    updated FormInput appearance

--- src/ui/atoms/FormInput.tsx
+++ src/ui/atoms/FormInput.tsx
@@ -14,7 +14,7 @@ export const FormInput = ({
 		<span className="text-sm font-bold text-gray-900">{label}</span>
 
 		<input
-			className="w-full appearance-none self-start rounded-md border-0 bg-white px-4 py-2 text-sm text-slate-400 ring-1 ring-inset ring-gray-400 placeholder:text-slate-400 focus:border-gray-900 focus:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-900 lg:min-w-4"
+			className="w-full appearance-none self-start rounded-md border-0 bg-white px-4 py-2 text-sm text-gray-950 ring-1 ring-inset ring-gray-400 placeholder:text-slate-400 focus:border-gray-900 focus:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-900 lg:min-w-4"
 			{...props}
 		/>
 
