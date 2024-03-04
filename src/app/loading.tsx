deb828a636475ff5b87d2c13e8b8725425e697b1
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2024-03-04 16:15:09 +0100

    refactored loading and not-found components

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import { Loader } from "@/ui/atoms/Loader";
 
 export default async function Loading() {
 	return (
-		<div className="flex h-[calc(100vh-4rem)] w-full flex-col items-center justify-center text-center">
+		<div className="flex w-full flex-col items-center justify-center pt-20 text-center">
 			<Loader />
 		</div>
 	);
