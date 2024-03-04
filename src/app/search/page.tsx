23210fb101b5b6f3a3e9b6d70e23067c62867522
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2024-03-04 22:11:18 +0100

    updated search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,7 @@
 import { type Metadata } from "next";
 
+import { SearchX } from "lucide-react";
+import Link from "next/link";
 import { PageHeading } from "@/ui/molecules/PageHeading";
 import { getProductList } from "@/api/products/getProductList";
 import { ProductsList } from "@/ui/organisms/ProductList";
@@ -32,7 +34,24 @@ export default async function SearchPage({ searchParams }: SearchPageType) {
 			/>
 
 			<section className="mx-auto max-w-md p-12 sm:max-w-2xl sm:py-16 md:max-w-4xl lg:max-w-7xl">
-				{products && <ProductsList products={products} />}
+				{products?.length ? (
+					<ProductsList products={products} />
+				) : (
+					<div className="flex w-full flex-col items-center justify-center pt-20 text-center">
+						<SearchX className="h-28 w-28 text-slate-500" />
+
+						<p className="my-2 text-2xl">
+							We could not find what you were looking for
+						</p>
+
+						<Link
+							href={{ pathname: "/" }}
+							className="text-sm uppercase hover:underline"
+						>
+							Back to shopping
+						</Link>
+					</div>
+				)}
 			</section>
 		</>
 	);
