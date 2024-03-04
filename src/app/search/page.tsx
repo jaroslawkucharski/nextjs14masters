535535ddafe5003b80abed1b62efe710c6b7a128
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2024-03-04 16:23:37 +0100

    refactored search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,10 +1,8 @@
-// TODO -pagination
 import { type Metadata } from "next";
-import { Suspense } from "react";
+
 import { PageHeading } from "@/ui/molecules/PageHeading";
 import { getProductList } from "@/api/products/getProductList";
 import { ProductsList } from "@/ui/organisms/ProductList";
-import { Loader } from "@/ui/atoms/Loader";
 
 export const metadata: Metadata = {
 	title: "Search",
@@ -20,7 +18,6 @@ export type SearchPageType = {
 export default async function SearchPage({ searchParams }: SearchPageType) {
 	const data = searchParams.query
 		? await getProductList({
-				take: 4,
 				search: searchParams.query,
 			})
 		: null;
@@ -35,11 +32,7 @@ export default async function SearchPage({ searchParams }: SearchPageType) {
 			/>
 
 			<section className="mx-auto max-w-md p-12 sm:max-w-2xl sm:py-16 md:max-w-4xl lg:max-w-7xl">
-				{products && (
-					<Suspense fallback={<Loader />}>
-						<ProductsList products={products} />
-					</Suspense>
-				)}
+				{products && <ProductsList products={products} />}
 			</section>
 		</>
 	);
