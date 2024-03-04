0b42ea6ca384a892ed5fc2e1b7c05bc3f79555eb
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2024-03-04 21:20:19 +0100

    refactored sorting logic and updated query parameters

--- src/app/products/[[...page]]/page.tsx
+++ src/app/products/[[...page]]/page.tsx
@@ -1,15 +1,15 @@
 import { getProductList } from "@/api/products/getProductList";
 import { ProductsList } from "@/ui/organisms/ProductList";
-import { DEFAULT_AMOUNT_OF_PRODUCTS } from "@/constants";
 import { type ProductSortBy, type SortDirection } from "@/gql/graphql";
+import { getSkip } from "@/helpers";
 
 export type ProductsPageType = {
 	params: {
 		page: string;
 	};
 	searchParams: {
-		sort: ProductSortBy;
-		by: SortDirection;
+		order: ProductSortBy;
+		dir: SortDirection;
 	};
 };
 
@@ -17,30 +17,12 @@ export default async function ProductsPage({
 	params,
 	searchParams,
 }: ProductsPageType) {
-	const skip =
-		Number(params.page) * DEFAULT_AMOUNT_OF_PRODUCTS -
-		DEFAULT_AMOUNT_OF_PRODUCTS;
-
-	const isValidProductSortName = (value: string): value is ProductSortBy => {
-		return ["DEFAULT", "NAME", "PRICE", "RATING"].includes(value);
-	};
-
-	const isValidProductSortBy = (value: string): value is ProductSortBy => {
-		return ["ASC", "DESC"].includes(value);
-	};
-
-	const sort = {
-		...(searchParams.sort &&
-			isValidProductSortName(searchParams.sort) && {
-				orderBy: searchParams.sort,
-			}),
-		...(searchParams.by &&
-			isValidProductSortBy(searchParams.by) && { order: searchParams.by }),
-	};
+	const skip = getSkip(params.page);
 
 	const { products } = await getProductList({
 		skip,
-		...sort,
+		order: searchParams.dir,
+		orderBy: searchParams.order,
 	});
 
 	return <ProductsList products={products} />;
--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,9 @@
+import { DEFAULT_AMOUNT_OF_PRODUCTS } from "./constants";
+
 export const getNumOfPages = (
 	numOfProducts: number,
 	amountOfProducts: number,
 ) => Math.ceil(numOfProducts / amountOfProducts);
+
+export const getSkip = (page: string) =>
+	Number(page) * DEFAULT_AMOUNT_OF_PRODUCTS - DEFAULT_AMOUNT_OF_PRODUCTS;
