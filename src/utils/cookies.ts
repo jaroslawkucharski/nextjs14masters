0c57a4f7f1a7824185c10587085ab024ed235b17
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2024-03-08 10:36:34 +0100

    created cookies and intl utility functions

---
+++ src/utils/cookies.ts
@@ -0,0 +1,19 @@
+"use server";
+
+import { cookies } from "next/headers";
+
+export const getCookie = async (key: string) => cookies().get(key)?.value;
+
+export const setCookie = async (key: string, value: string) =>
+	cookies().set(key, value, {
+		httpOnly: true,
+		sameSite: "strict",
+		maxAge: 60 * 60 * 24 * 7,
+	});
+
+export const removeCookie = async (key: string) =>
+	cookies().set(key, "", {
+		httpOnly: true,
+		sameSite: "strict",
+		expires: new Date(0),
+	});
