bc0d724d0066d5e7234296019f70f9ed3c20fbe0
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2024-03-10 14:32:30 +0100

    added sort translations

--- src/ui/molecules/PageHeading.tsx
+++ src/ui/molecules/PageHeading.tsx
@@ -1,4 +1,5 @@
 import { type ReactNode } from "react";
+import { getTranslations } from "next-intl/server";
 import { Sort } from "@/ui/molecules/Sort";
 
 type PageHeadingProps = {
@@ -8,21 +9,37 @@ type PageHeadingProps = {
 	sort?: boolean;
 };
 
-export const PageHeading = ({
+export const PageHeading = async ({
 	title,
 	description,
 	children,
 	sort,
-}: PageHeadingProps) => (
-	<div className="bg-slate-50 px-8 py-10 text-center sm:px-36 lg:text-left">
-		<div className="flex flex-wrap items-center justify-center gap-4 sm:justify-between">
-			{title && <h2 className="text-2xl font-semibold">{title}</h2>}
+}: PageHeadingProps) => {
+	const t = await getTranslations("Sort");
 
-			{sort && <Sort />}
+	return (
+		<div className="bg-slate-50 px-8 py-10 text-center sm:px-36 lg:text-left">
+			<div className="flex flex-wrap items-center justify-center gap-4 sm:justify-between">
+				{title && <h2 className="text-2xl font-semibold">{title}</h2>}
+
+				{sort && (
+					<Sort
+						i18n={{
+							sortBy: t("sortby"),
+							nameAsc: t("name-asc"),
+							nameDesc: t("name-desc"),
+							priceAsc: t("price-asc"),
+							priceDesc: t("price-desc"),
+							ratingAsc: t("rating-asc"),
+							ratingDesc: t("rating-desc"),
+						}}
+					/>
+				)}
+			</div>
+
+			{description && <p className="text-gray-600">{description}</p>}
+
+			{children && <div className="mt-8">{children}</div>}
 		</div>
-
-		{description && <p className="text-gray-600">{description}</p>}
-
-		{children && <div className="mt-8">{children}</div>}
-	</div>
-);
+	);
+};
