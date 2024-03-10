008246e28b3e544a94dc81bd81b01537bbbee0b5
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2024-03-10 19:56:27 +0100

    updated cart API

--- src/api/cart/getCartById.ts
+++ src/api/cart/getCartById.ts
@@ -1,18 +1,24 @@
 import { executeGraphQl } from "../graphqlApi";
 import { getCookie } from "@/utils/cookies";
-import { CartGetByIdDocument, type CartGetByIdQuery } from "@/gql/graphql";
+import {
+	CartGetByIdDocument,
+	type CartGetByIdQueryVariables,
+	type CartGetByIdQuery,
+} from "@/gql/graphql";
 
-export const getCartById = async (): Promise<CartGetByIdQuery["cart"]> => {
-	const cartId = await getCookie("cartId");
+export const getCartById = async (
+	id?: CartGetByIdQueryVariables["id"],
+): Promise<CartGetByIdQuery["cart"]> => {
+	const cartId = (await getCookie("cartId")) ?? id;
 
-	if (!cartId) {
+	if (!cartId && !id) {
 		return null;
 	}
 
 	const graphqlResponse = await executeGraphQl({
 		query: CartGetByIdDocument,
 		variables: {
-			id: cartId,
+			id: cartId ?? "",
 		},
 		cache: "no-store",
 		next: {
