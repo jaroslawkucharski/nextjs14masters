933681d84baa2bf14cd25a94c350a03af65d9af3
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2024-03-16 12:24:03 +0100

    refactored ActiveLink component styling

--- src/ui/atoms/ActiveLink.tsx
+++ src/ui/atoms/ActiveLink.tsx
@@ -31,13 +31,16 @@ export const ActiveLink = ({
 
 	return (
 		<Link
-			className={clsx(`text-lg text-gray-500 hover:text-gray-900`, {
-				["text-gray-900"]: isActive,
-				["pointer-events-none text-gray-400"]: isDisabled,
-				[className]: className,
-				[activeClassName]: isActive,
-				[disabledClassName]: isDisabled,
-			})}
+			className={clsx(
+				`text-lg text-gray-500 hover:text-gray-900`,
+				{
+					["text-gray-900"]: isActive,
+					["pointer-events-none text-gray-400"]: isDisabled,
+					[activeClassName]: isActive,
+					[disabledClassName]: isDisabled,
+				},
+				className,
+			)}
 			href={href}
 			aria-current={isActive || undefined}
 			aria-disabled={isDisabled}
