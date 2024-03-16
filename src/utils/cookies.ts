082dba8f2a4d7f9fa846003740fc0838158e3c81
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2024-03-16 12:23:42 +0100

    updated setCookie function to accept maxAge parameter

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -4,11 +4,11 @@ import { cookies } from "next/headers";
 
 export const getCookie = async (key: string) => cookies().get(key)?.value;
 
-export const setCookie = async (key: string, value: string) =>
+export const setCookie = async (key: string, value: string, maxAge?: number) =>
 	cookies().set(key, value, {
 		httpOnly: true,
 		sameSite: "strict",
-		maxAge: 60 * 60 * 24 * 7,
+		maxAge: maxAge ?? 60 * 60 * 24 * 7,
 	});
 
 export const removeCookie = async (key: string) => cookies().delete(key);
