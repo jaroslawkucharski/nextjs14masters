889bc2d1aa314ad39cff2858d3b6671367d7225f
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2024-03-20 09:24:05 +0100

    refactored SuccessView component to use orderId instead of intent

--- src/app/[locales]/cart/SuccessView/SuccessView.tsx
+++ src/app/[locales]/cart/SuccessView/SuccessView.tsx
@@ -1,21 +1,15 @@
 import { Package } from "lucide-react";
 import { getTranslations } from "next-intl/server";
 import Link from "next/link";
-import Stripe from "stripe";
+import { redirect } from "next/navigation";
+import { PATHS } from "@/constants";
 
-export const SuccessView = async ({ intent }: { intent: string }) => {
+export const SuccessView = async ({ orderId }: { orderId: string }) => {
 	const t = await getTranslations("SuccessCart");
 
-	if (!process.env.STRIPE_SECRET_KEY) {
-		throw new Error("Stripe secret key is missing");
+	if (!orderId) {
+		return redirect(PATHS.CART);
 	}
-
-	const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-		apiVersion: "2023-10-16",
-		typescript: true,
-	});
-
-	const paymentIntents = await stripe.paymentIntents.retrieve(intent);
 
 	return (
 		<div className="flex w-full flex-col items-center justify-center pt-20 text-center">
@@ -25,11 +19,9 @@ export const SuccessView = async ({ intent }: { intent: string }) => {
 
 			<p className="mt-2 text-2xl font-semibold">{t("order")}</p>
 
-			<p className="mb-2 text-2xl text-green-500">
-				{paymentIntents.metadata.orderId}
-			</p>
+			<p className="mb-2 text-2xl text-green-500">{orderId}</p>
 
-			<Link href="/orders" className="text-sm uppercase hover:underline">
+			<Link href={PATHS.ORDERS} className="text-sm uppercase hover:underline">
 				{t("back")}
 			</Link>
 		</div>
