1998b3b0717f5efc3c185c0b11e13bf0debf3397
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2024-03-20 20:14:07 +0100

    refactored SuccessView and StripeForm components

--- src/app/[locales]/cart/SuccessView/SuccessView.tsx
+++ src/app/[locales]/cart/SuccessView/SuccessView.tsx
@@ -2,28 +2,47 @@ import { Package } from "lucide-react";
 import { getTranslations } from "next-intl/server";
 import Link from "next/link";
 import { redirect } from "next/navigation";
-import { PATHS } from "@/constants";
+import { Suspense } from "react";
+import { CHECKOUT_STEPS, PATHS } from "@/constants";
+import { PageHeading } from "@/ui/molecules/PageHeading";
+import { checkoutSteps } from "@/utils/checkoutSteps";
+import { CheckoutTimeline } from "@/ui/atoms/CheckoutTimeline";
 
 export const SuccessView = async ({ orderId }: { orderId: string }) => {
-	const t = await getTranslations("SuccessCart");
+	const t = await getTranslations();
 
 	if (!orderId) {
 		return redirect(PATHS.CART);
 	}
 
+	const steps = await checkoutSteps(CHECKOUT_STEPS.SUMMARY);
+
 	return (
-		<div className="flex w-full flex-col items-center justify-center pt-20 text-center">
-			<Package className="h-28 w-28 text-green-500" />
+		<>
+			<PageHeading title={t("cart-summary")} />
 
-			<p className="my-2 text-2xl">{t("title")}</p>
+			<Suspense>
+				<CheckoutTimeline steps={steps} />
+			</Suspense>
 
-			<p className="mt-2 text-2xl font-semibold">{t("order")}</p>
+			<div className="flex w-full flex-col items-center justify-center pt-20 text-center">
+				<Package className="h-28 w-28 text-green-500" />
 
-			<p className="mb-2 text-2xl text-green-500">{orderId}</p>
+				<p className="my-2 text-2xl">{t("checkout-success-title")}</p>
 
-			<Link href={PATHS.ORDERS} className="text-sm uppercase hover:underline">
-				{t("back")}
-			</Link>
-		</div>
+				<p className="mt-2 text-2xl font-semibold">
+					{t("checkout-success-order")}
+				</p>
+
+				<p className="mb-2 text-2xl text-green-500">{orderId}</p>
+
+				<Link
+					href={`${PATHS.ORDER}/${orderId}`}
+					className="text-sm uppercase hover:underline"
+				>
+					{t("checkout-success-link")}
+				</Link>
+			</div>
+		</>
 	);
 };
--- src/app/[locales]/checkout/StripeForm/StripeForm.tsx
+++ src/app/[locales]/checkout/StripeForm/StripeForm.tsx
@@ -1,8 +1,14 @@
 "use client";
 
-import { Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
+import { AddressElement, Elements } from "@stripe/react-stripe-js";
+import {
+	type StripeAddressElementChangeEvent,
+	loadStripe,
+} from "@stripe/stripe-js";
+import { CornerDownLeft, Store } from "lucide-react";
+import { useState } from "react";
 import { CheckoutForm } from "../CheckoutForm";
+import { formatMoney } from "@/utils/intl";
 
 if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
 	throw new Error("Missing NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY env variable");
@@ -13,17 +19,95 @@ const stripePromise = loadStripe(
 );
 
 type StripeFormProps = {
-	clientSecret: string;
 	i18n: {
 		payNow: string;
+		productPrice: string;
+		delivery: string;
+		total: string;
+		freeReturns: string;
+		freeReturns30Days: string;
 	};
+	total?: number;
+	lang: string;
+	clientSecret: string;
 };
 
-export const StripeForm = ({ clientSecret, i18n }: StripeFormProps) => (
-	<Elements
-		options={{ appearance: { theme: "stripe" }, clientSecret }}
-		stripe={stripePromise}
-	>
-		<CheckoutForm i18n={i18n} />
-	</Elements>
-);
+export const StripeForm = ({
+	i18n,
+	total,
+	lang,
+	clientSecret,
+}: StripeFormProps) => {
+	const [totalAmount] = useState(total);
+
+	const [shipping, setShipping] = useState<
+		StripeAddressElementChangeEvent["value"]
+	>({
+		name: "",
+		address: {
+			line1: "",
+			line2: "",
+			city: "",
+			state: "",
+			postal_code: "",
+			country: "",
+		},
+	});
+
+	return (
+		<section className="mx-auto flex max-w-md flex-col gap-4 overflow-x-auto p-4 sm:max-w-2xl sm:p-12 sm:py-8 md:max-w-4xl lg:max-w-7xl lg:flex-row">
+			<Elements
+				options={{ appearance: { theme: "stripe" }, clientSecret }}
+				stripe={stripePromise}
+			>
+				<div className="flex w-full justify-center p-4 sm:p-10">
+					<AddressElement
+						options={{ mode: "shipping" }}
+						className="w-2/3"
+						onChange={(event) => {
+							setShipping(event.value);
+						}}
+					/>
+				</div>
+
+				<div className="min-w-full p-4 sm:min-w-[450px] sm:p-10">
+					<div className="mb-6">
+						<p className="flex w-full justify-between py-2 text-lg text-gray-500">
+							<span>{i18n.productPrice}</span>
+
+							<span>{formatMoney(Number(totalAmount), lang)}</span>
+						</p>
+
+						<p className="flex w-full justify-between py-2 text-lg text-gray-500">
+							<span>{i18n.delivery}</span>
+
+							<span>{formatMoney(Number(0), lang)}</span>
+						</p>
+
+						<p className="mt-4 flex w-full justify-between border-t py-2 text-lg">
+							<span>{i18n.total}</span>
+
+							<span>{formatMoney(Number(totalAmount), lang)}</span>
+						</p>
+					</div>
+
+					<CheckoutForm i18n={i18n} shipping={shipping} />
+
+					<div className="mt-8 flex flex-col gap-2">
+						<p className="flex items-center gap-2 text-sm text-slate-500">
+							<Store className="h-4 w-4" />
+
+							<span>{i18n.freeReturns}</span>
+						</p>
+
+						<p className="flex items-center gap-2 text-sm text-slate-500">
+							<CornerDownLeft className="h-4 w-4" />
+
+							<span>{i18n.freeReturns30Days}</span>
+						</p>
+					</div>
+				</div>
+			</Elements>
+		</section>
+	);
+};
