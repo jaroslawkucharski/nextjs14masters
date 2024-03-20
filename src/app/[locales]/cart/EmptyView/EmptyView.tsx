464585ddc8a95bcb36804125fe740a5c3d98e6f3
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2024-03-20 20:13:54 +0100

    added PageHeading component to EmptyView

--- src/app/[locales]/cart/EmptyView/EmptyView.tsx
+++ src/app/[locales]/cart/EmptyView/EmptyView.tsx
@@ -2,22 +2,27 @@ import { Shirt } from "lucide-react";
 import { getTranslations } from "next-intl/server";
 import Link from "next/link";
 import { PATHS } from "@/constants";
+import { PageHeading } from "@/ui/molecules/PageHeading";
 
 export const EmptyView = async () => {
-	const t = await getTranslations("EmptyCart");
+	const t = await getTranslations();
 
 	return (
-		<div className="flex w-full flex-col items-center justify-center pt-20 text-center">
-			<Shirt className="h-28 w-28 text-slate-500" />
+		<>
+			<PageHeading title={t("cart-title")} />
 
-			<p className="my-2 text-2xl">{t("title")}</p>
+			<div className="flex w-full flex-col items-center justify-center pt-20 text-center">
+				<Shirt className="h-28 w-28 text-slate-500" />
 
-			<Link
-				href={{ pathname: PATHS.HOME }}
-				className="text-sm uppercase hover:underline"
-			>
-				{t("back")}
-			</Link>
-		</div>
+				<p className="my-2 text-2xl">{t("empty-card-title")}</p>
+
+				<Link
+					href={{ pathname: PATHS.HOME }}
+					className="text-sm uppercase hover:underline"
+				>
+					{t("empty-card-link")}
+				</Link>
+			</div>
+		</>
 	);
 };
